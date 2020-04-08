7168940221ad1ac7963eb5c6dcdaf462ac4584df
DanielSLucas <daniellucas-pms@hotmail.com>
2020-04-08 10:08:48 -0300

    Functional

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
+import api from './services/api';
 import "./styles.css";
 
 function App() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, [])
 
   async function handleAddRepository() {
-    // TODO
+    const newRepo = {
+      title: `Tutorial ${Date.now()}`,
+	    url: "https://github.com/DanielSLucas/SequelizeSQL",
+	    techs: ["Node.js", "ExpressJS", "Sequelize ORM"]
+    }
+
+    const response = await api.post('repositories', newRepo);
+
+    setRepositories([...repositories, response.data]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter( repository => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {/* Listagem com base nos repositórios da api */}
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button> 
+          </li>
+        ))}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
